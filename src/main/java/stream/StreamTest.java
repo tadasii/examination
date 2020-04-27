5cc57a1fd42b2a486a11b5bf6aa3bf566b88e643
zhangzheng <517342427@qq.com>
2020-04-27 10:22:10 +0800

    增加delete

--- src/main/java/stream/StreamTest.java
+++ src/main/java/stream/StreamTest.java
@@ -1,27 +1,38 @@
 package stream;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
- * 实现 getDistinctStudents，getMaleStudents，sumScore，sortByScore，filterByScore五个方法
+ * 实现 deleteStudents,getDistinctStudents，getMaleStudents，sumScore，sortByScore，filterByScore六个方法
  * 注意：不要重写Student类的equals和hashCode方法
  */
 public class StreamTest {
+    /**
+     * 测试方法，写完后可以打开注释测试一下
+     * @param args
+     */
     public static void main(String[] args) {
         List<Student> studentList= getStudents();
-        studentList.forEach( student->{
+//        deleteStudents(studentList);
+//        studentList=  getDistinctStudents(studentList);
+//        studentList=   getMaleStudents(studentList);
+//        long sum= sumScore(studentList);
+//        System.out.println(sum);
+//        studentList= sortByScore(studentList);
+//        studentList=  filterByScore(600,studentList);
+        for (int i = 0; i < studentList.size(); i++) {
+            Student student = studentList.get(i);
             System.out.println("id=="+student.getId()+"male==="+student.getSex()+"score==="+student.getScore());
-        } );
+        }
     }
 
-    /** todo
-     * id重复的students去重
+    /**
+     * 删除 students中 name 等于 "bb" 的学生
      * @param students
-     * @return 返回去掉重复id的student的list
      */
-    public static List<Student> getDistinctStudents(List<Student> students){
-        //代码写在这里，并替换掉返回结果
-        return null ;
+    public static void deleteStudents(List<Student> students){
+
     }
 
     /**todo
@@ -66,17 +77,19 @@ public class StreamTest {
 
 
     public static List<Student> getStudents(){
-        List<Student> students =    Arrays.asList(
-                                        new Student("1","张三","男","1班",600),
-                                        new Student("2","李四","女","1班",620),
-                                        new Student("3","王五","男","2班",500),
-                                        new Student("4","赵六","男","2班",530),
-                                        new Student("5","田七","女","1班",700),
-                                        new Student("1","张三","男","1班",600),
-                                        new Student("2","李四","女","1班",620),
-                                        new Student("6","钱八","女","2班",430),
-                                        new Student("7","吴九","男","2班",300),
-                                        new Student("8","铁十","男","1班",400));
+        List<Student> students =   new ArrayList<>();
+        students.add( new Student("1","张三","男","1班",600));
+        students.add( new Student("2","李四","女","1班",620));
+        students.add( new Student("0","bb","男","1班",400));
+        students.add( new Student("0","bb","男","1班",400));
+        students.add( new Student("3","王五","男","2班",500));
+        students.add( new Student("4","赵六","男","2班",530));
+        students.add( new Student("5","田七","女","1班",700));
+        students.add( new Student("1","张三","男","1班",600));
+        students.add( new Student("2","李四","女","1班",620));
+        students.add( new Student("6","钱八","女","2班",430));
+        students.add( new Student("7","吴九","男","2班",300));
+        students.add( new Student("8","铁十","男","1班",400));
         return students;
     }
 }
